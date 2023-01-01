44ddff9eca36a8ac21b81f3ad0868e0160d7df2d
Soham Desai <83870602+SohamDesai1@users.noreply.github.com>
2023-01-02 00:09:29 +0530

    Update arrays.rs

--- src/arrays.rs
+++ src/arrays.rs
@@ -12,16 +12,4 @@ fn main(){
     // array of arrays
     let array: [[u8; 2]; 2] = [[1, 2], [3, 4]];
     println!("Array of arrays: {:?}", array);
-    // borrowing slice
-    let array: [u8; 5] = [1, 2, 3, 4, 5];
-    let slice: &[u8] = &array[1..3];
-    borrowing_slice(array, slice);
-}
-
-fn borrowing_slice(arr: [u8;5], slice: &[u8]) {
-    println!("Array: {:?}", arr);
-    println!("Slice: {:?}", slice);
-    println!("Length of array: {}", arr.len());
-    println!("Length of slice: {}", slice.len());
-
 }
