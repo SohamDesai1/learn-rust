523ba851df8b965e503ce6cecb2b4ba13b4dc4b7
SohamDesai1 <sohamdesai123@gmail.com>
2023-01-01 23:39:32 +0530

    different types of variables

---
+++ src/variables.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let unsigned_integer: u8 = 255;
+    println!("Unsigned integer: {}", unsigned_integer);
+    let signed_integer: i8 = -128;
+    println!("Signed integer: {}", signed_integer);
+    let float: f32 = 3.14;
+    println!("Float: {}", float);
+    let boolean: bool = true;
+    println!("Boolean: {}", boolean);
+    let character: char = 'a';
+    println!("Character: {}", character);
+    let emoji: char = "\u{1F600}";
+    let string: &str = "Hello, world!";
+    println!("String: {}", string);
+
+}
