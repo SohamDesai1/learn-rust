74e3f1fecece017067d8f901ceaf8479d4452d40
SohamDesai1 <sohamdesai123@gmail.com>
2023-01-01 23:43:35 +0530

    arrays

---
+++ src/arrays.rs
@@ -0,0 +1,16 @@
+fn main(){
+    let array: [u8; 5] = [1, 2, 3, 4, 5];
+    println!("Array: {:?}", array);
+    let array: [u8; 5] = [0; 5];
+    println!("Length of array: {}", array.len());
+    // slice
+    let slice: &[u8] = &array[1..3];
+    println!("Slice: {:?}", slice);
+    // array of strings
+    let array: [&str; 3] = ["Hello", "world", "!"];
+    println!("Array of strings: {:?}", array);
+    // array of arrays
+    let array: [[u8; 2]; 2] = [[1, 2], [3, 4]];
+    println!("Array of arrays: {:?}", array);
+
+}
