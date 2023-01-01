016245aba3ecf129b51eabfcf7306fba0112c239
SohamDesai1 <sohamdesai123@gmail.com>
2023-01-01 23:50:56 +0530

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    let number: u32 = 42;
+    println!("Number: {}", number);
+    println!("Is even: {}", is_even(number));
 }
+
+pub fn is_even(number: u32) -> bool {
+    // in rust for return boolean value we can use this syntax in which we dont put semicolon
+    number % 2 == 0
+}
+    
