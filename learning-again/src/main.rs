dcd95178a45ca1526e1cc639c3f3a0211da97c4d
SohamDesai1 <sohamdesai123@gmail.com>
2024-11-26 18:36:29 +0530

    Implementations of Structs

--- learning-again/src/main.rs
+++ learning-again/src/main.rs
@@ -1,17 +1,34 @@
-struct User {
-    name: String,
-    lastname: String,
-    age: u16,
+use std::f32::consts::PI;
+
+struct Circle {
+    radius: f32,
+    diameter: f32,
+}
+
+impl Circle {
+    fn circumference(&self) -> f32 {
+        self.diameter * PI
+    }
+
+    fn area(&self) -> f32 {
+        self.radius * self.radius * PI
+    }
+
+    fn eg() -> u32 {
+        return 1;
+    }
 }
 
 fn main() {
-    let user = User {
-        name: String::from("Soham"),
-        lastname: String::from("Desai"),
-        age: 22,
+    let circle = Circle {
+        radius: 16.0,
+        diameter: 32.0,
     };
 
-    println!("{}",user.name);
-    println!("{}",user.lastname);
-    println!("{}",user.age)
+    println!(
+        "The circumference of the circle is {}",
+        circle.circumference()
+    );
+    println!("The area of the circle is {}", circle.area());
+    println!("This is like a static fn which prints {}", Circle::eg());
 }
