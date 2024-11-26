6a0c9e433c4e47a2a4c980599481e54e7c6bd5a6
SohamDesai1 <sohamdesai123@gmail.com>
2024-11-26 18:57:17 +0530

    Enums

--- learning-again/src/main.rs
+++ learning-again/src/main.rs
@@ -1,34 +1,27 @@
-use std::f32::consts::PI;
-
-struct Circle {
-    radius: f32,
-    diameter: f32,
-}
-
-impl Circle {
-    fn circumference(&self) -> f32 {
-        self.diameter * PI
-    }
-
-    fn area(&self) -> f32 {
-        self.radius * self.radius * PI
-    }
-
-    fn eg() -> u32 {
-        return 1;
-    }
+enum Moves {
+    // Type , power
+    Punch(String, u8),
+    Jab(String, u8),
+    Kick(String, u8),
+    Block(String, u8),
 }
 
 fn main() {
-    let circle = Circle {
-        radius: 16.0,
-        diameter: 32.0,
-    };
+    let punch = Moves::Punch(String::from("attack"), 20);
+    let jab = Moves::Jab(String::from("attack"), 40);
+    let kick = Moves::Kick(String::from("attack"), 35);
+    let block = Moves::Block(String::from("defense"), 20);
+    calc(punch);
+    calc(jab);
+    calc(kick);
+    calc(block);
+}
 
-    println!(
-        "The circumference of the circle is {}",
-        circle.circumference()
-    );
-    println!("The area of the circle is {}", circle.area());
-    println!("This is like a static fn which prints {}", Circle::eg());
+fn calc(moves: Moves) {
+    match moves {
+        Moves::Block(a, b) => println!("This is an {} move with power {}", a, b),
+        Moves::Punch(a, b) => println!("This is an {} move with power {}", a, b),
+        Moves::Jab(a, b) => println!("This is an {} move with power {}", a, b),
+        Moves::Kick(a, b) => println!("This is an {} move with power {}", a, b),
+    }
 }
