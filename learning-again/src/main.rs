04d00fb7d46be521a8b9b1152cb7870356ad59e0
SohamDesai1 <sohamdesai123@gmail.com>
2024-11-26 18:10:36 +0530

    Strings

--- learning-again/src/main.rs
+++ learning-again/src/main.rs
@@ -1,23 +1,9 @@
 fn main() {
-    let ans = fib(6);
-    print!("{}", ans);
+    let name = String::from("Soham");
+    let len = get_str_len(name);
+    println!("The length is {}",len);
 }
 
-fn fib(num: u32) -> u32 {
-    let mut first = 0;
-    let mut second = 1;
-
-    if num == 0 {
-        return first;
-    }
-    if num == 1 {
-        return second;
-    }
-
-    for _ in 0..(num - 1) {
-        let temp = second;
-        second = second + first;
-        first = temp;
-    }
-    return second;
+fn get_str_len(s: String) -> usize {
+    s.chars().count()
 }
