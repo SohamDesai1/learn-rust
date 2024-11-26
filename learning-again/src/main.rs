e73db436b6c983bd334e176703f77d96b37a75fd
SohamDesai1 <sohamdesai123@gmail.com>
2024-11-26 17:54:33 +0530

    understanding functions and vars

--- learning-again/src/main.rs
+++ learning-again/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let ans = is_even(202);
+    print!("{}", ans);
+}
+
+fn is_even(num: u32) -> bool {
+    if num % 2 == 0 {
+        return true;
+    } else {
+        return false;
+    }
 }
