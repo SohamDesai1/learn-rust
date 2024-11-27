7feb969eb1a4de1492d0c01c276dd5506b98f3d5
SohamDesai1 <sohamdesai123@gmail.com>
2024-11-27 11:06:26 +0530

    Results

--- learning-again/src/main.rs
+++ learning-again/src/main.rs
@@ -1,16 +1,12 @@
+use std::fs::read_to_string;
+
 fn main() {
-    let index = find_first_a(String::from("Soham"));
-    match index {
-        Some(val) => println!("{}", val+1),
-        None => println!("Not found"),
+    let res = read_to_string("demo.txt");
+
+    match res {
+        Ok(data) => println!("{:?}",data),
+        Err(err) => println!("Error occured {}",err)
     }
+
 }
 
-fn find_first_a(s: String) -> Option<u8> {
-    for (index, char) in s.chars().enumerate() {
-        if char == 'a' {
-            return Some(index as u8);
-        }
-    }
-    return None;
-}
