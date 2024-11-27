0db5f1b95960b577fe42f857ce1647793ab042d4
SohamDesai1 <sohamdesai123@gmail.com>
2024-11-27 13:26:13 +0530

    Mutable reference in borrowing

--- learning-again/src/borrowing.rs
+++ learning-again/src/borrowing.rs
@@ -1,14 +1,16 @@
-fn print_str(s:&String) {
+fn print_str(s:&mut String) {
+    s.push_str(" desai");
     println!("{}",s)
 }
 
 fn main() {
-    let s = String::from("soham");
-    print_str(&s);
-    
+    let mut s = String::from("soham");
+    print_str(&mut s);
+
     //OR
-    let a = String::from("Soham");
-    let b = &a;
-    println!("{}",a);
-    println!("{}",b);
+    let mut a =  String::from("Soham Desai");
+    let b = &mut a;
+    // let c = &mut a; // Gives error because the rule says that no 2 mutable var 
+    let c = &b;
+    println!("{},{}",b,c)
 }
