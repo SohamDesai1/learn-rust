bcc843e900d0b390d148489938e7d03c292c0ef8
SohamDesai1 <sohamdesai123@gmail.com>
2024-11-27 10:55:20 +0530

    Options

--- learning-again/src/main.rs
+++ learning-again/src/main.rs
@@ -1,27 +1,16 @@
-enum Moves {
-    // Type , power
-    Punch(String, u8),
-    Jab(String, u8),
-    Kick(String, u8),
-    Block(String, u8),
-}
-
 fn main() {
-    let punch = Moves::Punch(String::from("attack"), 20);
-    let jab = Moves::Jab(String::from("attack"), 40);
-    let kick = Moves::Kick(String::from("attack"), 35);
-    let block = Moves::Block(String::from("defense"), 20);
-    calc(punch);
-    calc(jab);
-    calc(kick);
-    calc(block);
+    let index = find_first_a(String::from("Soham"));
+    match index {
+        Some(val) => println!("{}", val+1),
+        None => println!("Not found"),
+    }
 }
 
-fn calc(moves: Moves) {
-    match moves {
-        Moves::Block(a, b) => println!("This is an {} move with power {}", a, b),
-        Moves::Punch(a, b) => println!("This is an {} move with power {}", a, b),
-        Moves::Jab(a, b) => println!("This is an {} move with power {}", a, b),
-        Moves::Kick(a, b) => println!("This is an {} move with power {}", a, b),
+fn find_first_a(s: String) -> Option<u8> {
+    for (index, char) in s.chars().enumerate() {
+        if char == 'a' {
+            return Some(index as u8);
+        }
     }
+    return None;
 }
