cd206c39ae4767d30edb86067c8414dcf479a09e
SohamDesai1 <sohamdesai123@gmail.com>
2024-11-27 13:29:05 +0530

    completed Part 1

--- learning-again/src/main.rs
+++ learning-again/src/main.rs
@@ -1,12 +1,16 @@
-use std::fs::read_to_string;
-
-fn main() {
-    let res = read_to_string("demo.txt");
-
-    match res {
-        Ok(data) => println!("{:?}",data),
-        Err(err) => println!("Error occured {}",err)
-    }
-
+fn print_str(s:&mut String) {
+    s.push_str(" desai");
+    println!("{}",s)
 }
 
+fn main() {
+    let mut s = String::from("soham");
+    print_str(&mut s);
+
+    //OR
+    let mut a =  String::from("Soham Desai");
+    let b = &mut a;
+    // let c = &mut a; // Gives error because the rule says that no 2 mutable var 
+    let c = &b;
+    println!("{},{}",b,c)
+}
