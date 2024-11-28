7a94567e4e142e1927fcb35df4b22a7661707132
SohamDesai1 <sohamdesai123@gmail.com>
2024-11-28 16:50:19 +0530

    Structs with lifetimes

--- learning-again/src/lifetimes.rs
+++ learning-again/src/lifetimes.rs
@@ -1,3 +1,7 @@
+struct User<'a> {
+    name: &'a str,
+}
+
 fn main() {
     let ans;
     let str1 = String::from("Soham");
@@ -7,6 +11,10 @@ fn main() {
         println!("{}", ans)
     }
     // println!("{}", ans) // Error as str2 lifetime not long enough
+
+    let name = String::from("Soham");
+    let user = User { name: &name };
+    println!("{}", user.name);
 }
 
 fn longest<'a>(str1: &'a str, str2: &'a str) -> &'a str {
