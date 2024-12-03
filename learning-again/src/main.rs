e3a44478bff2b3cd5ed78752b424567222539b61
SohamDesai1 <sohamdesai123@gmail.com>
2024-12-03 08:59:51 +0530

    using tokio runtime

--- learning-again/src/main.rs
+++ learning-again/src/main.rs
@@ -1,16 +1,27 @@
+use std::time::Duration;
+use tokio::time::sleep;
+
 #[tokio::main]
-async fn main(){
-    let a = my_fn();
-    println!("Demo");
-    a.await;
+async fn main() {
+    let mut handles = vec![];
+    for i in 0..2 {
+        let handle = tokio::spawn(async move {
+            my_fn(i).await;
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.await.unwrap();
+    }
 }
 
-async fn my_fn() {
+async fn my_fn(i: i32) {
     let s1 = demo().await;
-    println!("First {s1}");
+    println!("First {s1} and {i}");
     let s2 = demo().await;
-    println!("Second {s2}");
+    println!("Second {s2} and {i}");
 }
-async fn demo() ->String{
+async fn demo() -> String {
+    sleep(Duration::from_millis(100)).await;
     "Demo".to_owned()
 }
